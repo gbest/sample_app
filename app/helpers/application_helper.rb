86ad23b52123b4e324829a2021f9a5dc92ac7403
gbest <graham@tightropedream.com>
2011-11-30 10:41:18 -0800

    Finished tutorial 4.3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+ # tutorial: Listing 4.2
+ # Return a title on a per-page basis.
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if !@title.nil?
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
